c2875fc996d68b108ec32fe195fdac42dc1098e1
lakshmanan <lakshman@SHRIGROUPIDC.COM>
2022-11-01 13:19:50 +0530

    first commit

---
+++ includes/config.php
@@ -0,0 +1,16 @@
+<?php 
+// DB credentials.
+define('DB_HOST','employee-test.cblvjt20yyg6.us-east-1.rds.amazonaws.com');
+define('DB_USER','admin');
+define('DB_PASS','password123');
+define('DB_NAME','employee');
+// Establish database connection.
+try
+{
+$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+}
+catch (PDOException $e)
+{
+exit("Error: " . $e->getMessage());
+}
+?>
